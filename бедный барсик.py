fea8de43f840c39df11171969fd27ce10937e916
NeRuDiK <95687155+NeRuDiK@users.noreply.github.com>
2022-05-08 14:41:53 +0500

    Update бедный барсик.py

--- бедный барсик.py
+++ бедный барсик.py
@@ -10,7 +10,7 @@ class BarsikMod(loader.Module):
 	async def watcher(self, message):
 		me = (await message.client.get_me())
 		client1 = self.client
-		result = "Бедный Бусик"
+		result = "Бедный Барсик"
 		if message.sender_id != me.id:
 			if message.text.lower() == "бб":
 				await message.respond(result)
